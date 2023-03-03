407757a6910bdad893a80956af8d72028da19c21
Charlie <charliealders26@gmail.com>
2023-03-03 12:11:26 -0800

    Update wowhead_bis.py

--- wowhead_bis.py
+++ wowhead_bis.py
@@ -3,34 +3,7 @@ import requests
 import re
 import numpy as np
 from prettytable import PrettyTable
-
-available_slots = ['weapons', 'trinkets', 'head', 'neck', 'shoulders', 'back', 'chests', 'wrists', 'hands', 'waists', 'legs', 'feet', 'rings']
-available_specs = [
-    'blood-death-knight', 'frost-death-knight', 'unholy-death-knight',
-    'havoc-demon-hunter', 'vengeance-demon-hunter',
-    'balance-druid', 'feral-druid', 'guardian-druid', 'restoration-druid',
-    'beast-mastery-hunter', 'marksmanship-hunter', 'survival-hunter',
-    'arcane-mage', 'fire-mage', 'frost-mage',
-    'brewmaster-monk', 'mistweaver-monk', 'windwalker-monk',
-    'holy-paladin', 'protection-paladin', 'retribution-paladin',
-    'discipline-priest', 'holy-priest', 'shadow-priest',
-    'assassination-rogue', 'outlaw-rogue', 'subtlety-rogue',
-    'elemental-shaman', 'enhancement-shaman', 'restoration-shaman',
-    'affliction-warlock', 'demonology-warlock', 'destruction-warlock',
-    'arms-warrior', 'fury-warrior', 'protection-warrior',
-    'devastation-evoker', 'preservation-evoker'
-]
-
-available_bosses = [
-    'eranog',
-    'terros',
-    'the-primal-council',
-    'sennarth-the-cold-breath',
-    'dathea-ascended',
-    'kurog-grimtotem',
-    'broodkeeper-diurna',
-    'raszageth-the-storm-eater'
-]
+from variables import available_bosses, available_slots, available_specs
 
 def find_gear(subcreation_link):
     all_links = []
