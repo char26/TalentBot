5acd3444cba7ae77afe3067f2092a36e779c84c2
Charlie <charliealders26@gmail.com>
2023-03-03 12:08:56 -0800

    Moved love variables to different files

---
+++ variables.py
@@ -0,0 +1,26 @@
+available_slots = ['weapons', 'trinkets', 'head', 'neck', 'shoulders', 'back', 'chests', 'wrists', 'hands', 'waists', 'legs', 'feet', 'rings']
+available_specs = [
+    'blood-death-knight', 'frost-death-knight', 'unholy-death-knight',
+    'havoc-demon-hunter', 'vengeance-demon-hunter',
+    'balance-druid', 'feral-druid', 'guardian-druid', 'restoration-druid',
+    'beast-mastery-hunter', 'marksmanship-hunter', 'survival-hunter',
+    'arcane-mage', 'fire-mage', 'frost-mage',
+    'brewmaster-monk', 'mistweaver-monk', 'windwalker-monk',
+    'holy-paladin', 'protection-paladin', 'retribution-paladin',
+    'discipline-priest', 'holy-priest', 'shadow-priest',
+    'assassination-rogue', 'outlaw-rogue', 'subtlety-rogue',
+    'elemental-shaman', 'enhancement-shaman', 'restoration-shaman',
+    'affliction-warlock', 'demonology-warlock', 'destruction-warlock',
+    'arms-warrior', 'fury-warrior', 'protection-warrior'
+]
+
+available_bosses = [
+    'eranog',
+    'terros',
+    'the-primal-council',
+    'sennarth-the-cold-breath',
+    'dathea-ascended',
+    'kurog-grimtotem',
+    'broodkeeper-diurna',
+    'raszageth-the-storm-eater'
+]
